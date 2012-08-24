b0fd4ded7c6fdac733e163c349c70ee2e652d1b1
Heath Brown <heathd.brown@gmail.com>
2012-08-23 22:58:23 -0500

    Added lab script with procedures

--- cisco/bin/lab.pl
+++ cisco/bin/lab.pl
@@ -5,7 +5,10 @@
 #
 use strict;
 use warnings;
-
+use local::lib;
+use IO::Prompt;
+use Net::SSH::Expect;
+#use local::lib "$IO::Prompt";
 #   - Ask how we want to connect
 #	- ssh
 #	- console
@@ -38,3 +41,53 @@ use warnings;
 #		- backup
 #		- overwrite old config
 #			- Use original configuraiton to overwrite
+
+####Actual code
+my $username = prompt('Username: ');
+my $password = prompt('Password: ', -echo => '*');
+my $device = prompt('Device to connect: ');
+my $tftphost = "";
+#
+my $ssh = Net::SSH::Expect-> new (
+    host => $device,
+    password=> $password,
+    user => $username,
+#    exp_debug => 1,
+    ssh_option => '-o StrictHostKeyChecking=no',
+    timeout => 10,
+    log_file => "$device.ssh.log",
+    raw_pty => 1
+);
+eval {my $login_output = $ssh->login()};
+if ($@) {
+    print "SSH error\n$@\n";
+    print LOG "SSH error \n";
+}
+#
+eval {codeupgrade()};
+eval {backup()};
+#
+sub codeupgrade
+{
+    print "connected to $device\n";
+    my $text = $ssh ->exec("sh ver | include image");
+    $ssh ->waitfor('#', 5, -ex);
+    print $text;
+    return;
+}
+sub backup 
+{
+    $ssh->send("copy run tftp://$tftphost/$device-conf");
+    $ssh->waitfor('?', 5, -ex);
+    $ssh->send("\n");
+    $ssh->waitfor('?', 5, -ex);
+    $ssh->send("\n");
+    $ssh->waitfor('#', 5, -ex);
+    return;
+}
+sub pushnewconfig
+{
+    my $newconfig = prompt("Path to newconfig: ");
+    $ssh->send("copy tftp://$tftphost/$newconfig run");
+    return;
+}
